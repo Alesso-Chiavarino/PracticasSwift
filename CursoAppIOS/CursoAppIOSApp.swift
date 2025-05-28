e56e2e9ab62cc2a4382cc53a1ba57f360f696259
Alessandro Chiavarino <alessochiavarino@gmail.com>
2025-05-28 19:53:03 -0300

    Initial Commit

---
+++ CursoAppIOS/CursoAppIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoAppIOSApp.swift
+//  CursoAppIOS
+//
+//  Created by Alessandro Chiavarino on 28/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct CursoAppIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CursoAppIOSTests/CursoAppIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  CursoAppIOSTests.swift
+//  CursoAppIOSTests
+//
+//  Created by Alessandro Chiavarino on 28/05/2025.
+//
+
+import Testing
+@testable import CursoAppIOS
+
+struct CursoAppIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
