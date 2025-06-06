080a5afdd248ec4273d7647d224500cad257e7fa
Alessandro Chiavarino <alessochiavarino@gmail.com>
2025-06-06 15:57:42 -0300

    create hero app

---
+++ CursoAppIOS/Apps/SuperHeroFinder/SuperHeroFinderView.swift
@@ -0,0 +1,18 @@
+//
+//  SuperHeroFinderView.swift
+//  CursoAppIOS
+//
+//  Created by Alessandro Chiavarino on 06/06/2025.
+//
+
+import SwiftUI
+
+struct SuperHeroFinderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SuperHeroFinderView()
+}
---
+++ CursoAppIOS/Apps/SuperHeroFinder/services/SuperHero/SuperHero.service.swift
@@ -0,0 +1,8 @@
+//
+//  SuperHeroService.swift
+//  CursoAppIOS
+//
+//  Created by Alessandro Chiavarino on 06/06/2025.
+//
+
+import Foundation
